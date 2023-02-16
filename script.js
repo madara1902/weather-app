12fd3b2855020868de810fc2e470ed98892f5563
madara1902 <40071873+madara1902@users.noreply.github.com>
2023-02-16 20:42:52 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+// JavaScript Document
+const input = document.getElementById('input') 
+const text = document.querySelector('.card-text')
+const button = document.getElementById('btn')
+const icon = document.querySelector('.icon')
+
+button.addEventListener('click', function(){
+	const key = '4484bf48f3c94a38b0e114536231402'
+	fetch(`http://api.weatherapi.com/v1/current.json?key=${key}&q=${input.value}&aqi=yes}`, {
+		"method" : "GET",
+		"headers" : {
+			
+		}
+	})
+	.then(response => response.json())
+	.then(displayData)
+	.catch(err => alert("Wrong region name!"))
+})
+
+
+const displayData = (weather) => {	
+const icons = `<img src="https://img.icons8.com/emoji/96/null/sun-behind-cloud.png"/>`;
+
+icon.innerHTML = `<h2>${icons}</h2>`
+
+	text.innerHTML = `
+	<ul> 
+		<li> <p class="card-text">Region: ${weather.location.region}</p> </li>
+ <li><p class="card-text">Temperature: ${weather.current.temp_c} C</p></li>
+ <li><p class="card-text">mph: ${weather.current.wind_mph}</p></li>
+ <li><p class="card-text">Time: ${weather.location.localtime}</p></li>
+ </ul>
+    `
+}
