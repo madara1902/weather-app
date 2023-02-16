a8b51b23729e63992a4f715e83fff415536c6895
madara1902 <40071873+madara1902@users.noreply.github.com>
2023-02-16 22:14:40 +0700

    Update script.js

--- script.js
+++ script.js
@@ -7,8 +7,8 @@ const icon = document.querySelector('.icon')
 
 header('Access-Control-Allow-Origin: *')
 header('Access-Control-Allow-Headers: *')
-header('Access-Control-Allow-Methods: GET')
-header('Access-Control-Allow-credentials: true')
+header('Access-Control-Allow-Methods: *')
+header('Access-Control-Allow-Credentials: true')
 
 button.addEventListener('click', function(){
 	const key = '4484bf48f3c94a38b0e114536231402'
